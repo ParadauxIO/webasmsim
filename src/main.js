85418efc1e6a140b08f4af57c0ae60c1a989edc2
Rían Errity <rian@paradaux.io>
2021-08-05 22:49:04 +0100

    Add codemirror

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/codemirror/codemirror.min.css'
+import './assets/codemirror/codemirror.min'
 
 createApp(App).use(router).mount('#app')
