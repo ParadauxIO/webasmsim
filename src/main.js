870ee5504391fc80e92827b14955da2373930355
Rían Errity <rian@paradaux.io>
2021-08-09 02:34:44 +0100

    Add bulma

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 import './assets/codemirror/codemirror.min.css'
 import './assets/codemirror/codemirror.min'
+import './assets/bulma/bulma.min.css'
 
 createApp(App).use(router).mount('#app')
